0e70ead7f84fa5e2bbd70e2c9ffc91fc85ed4a1f
Daniel Kraus <daniel@kraus.my>
2019-02-28 15:28:03 +0000

    Rewrite demo with let-alist and add cl-assert statement

--- sodium-box-demo.el
+++ sodium-box-demo.el
@@ -7,27 +7,25 @@
 ;; Simple demonstration of the crypto_box API
 
 ;;; Code:
-
+(eval-when-compile
+  (require 'cl-lib))
 (require 'sodium)
 
 (defun sodium-box-demo ()
   "Simple demonstration of the crypto_box API."
   (pop-to-buffer (messages-buffer))
-  (let* ((nonce  (sodium-box-make-nonce))
-         (bob    (sodium-box-keypair))
-         (bob-pk (cdr (assoc 'pk bob)))
-         (bob-sk (cdr (assoc 'sk bob)))
-         (alice    (sodium-box-keypair))
-         (alice-pk (cdr (assoc 'pk alice)))
-         (alice-sk (cdr (assoc 'sk alice)))
-         (msg "Hello World!")
-         encrypted decrypted)
-    (message "Encrypting message: '%s'" msg)
-    (setq encrypted (sodium-box msg nonce bob-pk alice-sk))
-    (message "Decrypt message:    '%s'" encrypted)
-    (setq decrypted (sodium-box-open encrypted nonce alice-pk bob-sk))
-    (message "Decrypted message:  '%s'" decrypted)
-    (message "Nonce:              '%s'" nonce)
-    (message "Incremented nonce:  '%s'" (sodium-increment nonce))))
+  (let ((nonce  (sodium-box-make-nonce))
+        (msg "Hello World!")
+        encrypted decrypted)
+    (let-alist `((alice . ,(sodium-box-keypair))
+                 (bob   . ,(sodium-box-keypair)))
+      (message "Encrypting message: '%s'" msg)
+      (setq encrypted (sodium-box msg nonce .bob.pk .alice.sk))
+      (message "Decrypt message:    '%s'" encrypted)
+      (setq decrypted (sodium-box-open encrypted nonce .alice.pk .bob.sk))
+      (message "Decrypted message:  '%s'" decrypted)
+      (cl-assert (string-equal msg decrypted))
+      (message "Nonce:              '%s'" nonce)
+      (message "Incremented nonce:  '%s'" (sodium-increment nonce)))))
 
 ;;; sodium-box-demo.el ends here
